da6ee4621cde13f89dc4c13037c507fc32f2ca6e
theblakman871-source <theblakman871@gmail.com>
2026-08-26 23:15:47 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-// Fyll i dessa två värden när Supabase-projektet är skapat.
-// Anon-nyckeln är avsedd att kunna finnas i frontend. RLS skyddar datan.
-window.BLENDXO_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
-  mode: 'demo' // ändra till 'supabase' när ovanstående är ifyllt
-};
